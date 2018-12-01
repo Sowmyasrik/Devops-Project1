41686c8984a5a018d7b4c4f724f7240e7b19d65d
sowmyasri95 <45523002+sowmyasri95@users.noreply.github.com>
2018-12-01 18:24:42 -0500

    Add files via upload

---
+++ Models/Manufacturer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartsUnlimited.Models
+{
+    public class Manufacturer
+    {
+        public int ManufacturerId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        // Some Comment to trigger a Build
+        // Some more changes...
+    }
+}
